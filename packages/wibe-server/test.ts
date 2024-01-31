1b125d12e2d9f51a1fc19c22958654d69c79c9f6
coratgerl <mr.palixir@gmail.com>
2024-01-31 20:12:24 +0100

    feat: add schema creation (need tests, need move to package)

--- packages/wibe-server/test.ts
+++ packages/wibe-server/test.ts
@@ -1,19 +1,23 @@
 import { gql } from 'graphql-request'
 
 const run = async () => {
-    const res = await fetch('http://localhost:3000/graphql', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-            query: gql`
+  const res = await fetch('http://localhost:3000/graphql', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      query: gql`
                 query IntrospectionSchema {
                     __schema {
                         types {
                             name
                             description
                             kind
+                            enumValues {
+                                name
+                                description
+                            }
                             inputFields {
                                 name
                                 type {
@@ -22,60 +26,23 @@ const run = async () => {
                                     ofType {
                                         name
                                         kind
+                                        ofType{
+                                          name
+                                          kind
+                                        }
                                     }
                                 }
                             }
                             fields {
                                 name
                                 description
-                                args {
-                                    name
-                                    description
-                                    type {
-                                        name
-                                        kind
-                                        ofType {
-                                            name
-                                            kind
-                                        }
-                                    }
-                                }
-                            }
-                        }
-                        mutationType {
-                            name
-                            description
-                            fields {
-                                name
-                                description
-                                args {
-                                    name
-                                    description
-                                    type {
-                                        name
-                                        kind
-                                        ofType {
-                                            name
-                                            kind
-                                        }
-                                    }
-                                }
                                 type {
                                     name
                                     kind
                                     ofType {
                                         name
-                                        kind
                                     }
                                 }
-                            }
-                        }
-                        queryType {
-                            name
-                            description
-                            fields {
-                                name
-                                description
                                 args {
                                     name
                                     description
@@ -86,14 +53,6 @@ const run = async () => {
                                             name
                                             kind
                                         }
-                                    }
-                                }
-                                type {
-                                    name
-                                    kind
-                                    ofType {
-                                        name
-                                        kind
                                     }
                                 }
                             }
@@ -101,56 +60,79 @@ const run = async () => {
                     }
                 }
             `,
-        }),
+    }),
+  })
+
+  const {
+    data: {
+      __schema: { queryType, mutationType, types: objectTypes },
+    },
+  } = await res.json()
+
+  const scalars = objectTypes.filter((type: any) => !type.name.startsWith('__') && type.kind === 'SCALAR').map((type: any) => {
+    return `scalar ${type.name}`
+  })
+
+  const enums = objectTypes.filter((type: any) => !type.name.startsWith('__') && type.kind === 'ENUM').map((type: any) => {
+    const fields = type.enumValues.map((value: any) => {
+      return `${value.name}`
     })
 
-    const {
-        data: {
-            __schema: { queryType, mutationType, types: objectTypes },
-        },
-    } = await res.json()
+    return `enum ${type.name} {\n\t${fields.join(',\n\t')}\n}`
+  })
 
-    const types = objectTypes.map((type: any) => {
-        const fields = type.fields?.map((field: any) => {
-            const args = field.args.map((arg: any) => {
-                return `${arg.name}: ${arg.type.name}`
-            })
-        })
 
-        if (type.kind === 'SCALAR' || type.kind === 'ENUM') {
-            return `${type.name}`
-        }
+  const types = objectTypes.filter((type: any) => type.kind !== 'ENUM' && type.kind !== 'SCALAR' && !type.name.startsWith('__')).map((type: any) => {
+    const description = `${type.description ? '"""\n' : ''}${type.description ? type.description : ''}${type.description ? '"""\n' : ''}`
 
-        if (type.kind === 'INPUT_OBJECT') {
-            const fields = type.inputFields.map((field: any) => {
-                return `${field.name}: ${field.type.name}`
-            })
-
-            return `${type.name} { ${fields.join(', ')} }`
-        }
-
-        return `${type.name} { ${fields.join(', ')} }`
-    })
-
-    console.log(types)
-
-    const queries = queryType.fields.map((field: any) => {
+    if (type.name === 'Query' || type.name === 'Mutation') {
+      const queries = type.fields.map((field: any) => {
         const args = field.args.map((arg: any) => {
-            return `${arg.name}: ${arg.type.name}`
+          return `${arg.name}: ${arg.type.name ? arg.type.name : arg.type.ofType.name}${arg.type.kind === 'NON_NULL' ? '!' : ''}`
         })
 
-        return `${field.name}(${args.join(', ')}): ${field.type.name === null ? field.type.ofType.name : field.type.name}${field.type.kind === 'NON_NULL' ? '!' : ''}`
+        return `${field.name}(${args.join(', ')}): ${field.type.name ? field.type.name : field.type.ofType.name}${field.type.kind === 'NON_NULL' ? '!' : ''}`
+      })
+
+      return `${description}type ${type.name} {\n\t${queries.join('\n\t')}\n}`
+    }
+
+    if (type.kind === 'INPUT_OBJECT') {
+      const getTypeName = (field: any) => {
+        if (field.type.ofType.name)
+          return field.type.ofType.name
+
+        return field.type.ofType.ofType.name
+      }
+
+      const fields = type.inputFields.map((field: any) => {
+        return `${field.name}: ${field.type.name ? field.type.name : ('[' + getTypeName(field) + ']')}${field.type.kind === 'NON_NULL' ? '!' : ''}`
+      })
+
+      return `${description}input ${type.name} { \n\t${fields.join(',\n\t')} \n} `
+    }
+
+    if (type.kind === 'OBJECT') {
+      const fields = type.fields.map((field: any) => {
+        return `${description}${field.name}: ${field.type.name ? field.type.name : field.type.ofType.name}${field.type.kind === 'NON_NULL' ? '!' : ''}`
+      })
+
+      return `${description}type ${type.name
+        } { \n\t${fields.join(',\n\t')}\n}`
+    }
+
+    const fields = type.fields?.map((field: any) => {
+      const args = field.args.map((arg: any) => {
+        return `${arg.name}: ${arg.type.name} `
+      })
     })
 
-    const mutations = mutationType.fields.map((field: any) => {
-        const args = field.args.map((arg: any) => {
-            return `${arg.name}: ${arg.type.name}`
-        })
+    return `${type.name} { ${fields.join(', ')} } `
+  })
 
-        return `${field.name}(${args.join(', ')}): ${field.type.name === null ? field.type.ofType.name : field.type.name}${field.type.kind === 'NON_NULL' ? '!' : ''}`
-    })
+  await Bun.write('schema.graphql', `${scalars.join('\n')} \n\n${enums.join('\n\n')} \n\n${types.join('\n\n')} `)
 }
 
 run().catch((err) => {
-    console.error(err)
+  console.error(err)
 })
