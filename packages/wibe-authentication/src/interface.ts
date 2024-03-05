54d82f503ea432f407659df36b2ec8ea7af0ec01
coratgerl <73360179+coratgerl@users.noreply.github.com>
2024-03-05 18:20:40 +0100

    fix: remove wibe-authentication

--- packages/wibe-authentication/src/interface.ts
+++
@@ -1,9 +0,0 @@
-export interface AuthenticationEventsOptions {
-  user: _User
-  input: Record<string, any>
-  context: Context
-}
-
-export interface AuthentiationInterface {
-  onLogin: (input: )
-}
