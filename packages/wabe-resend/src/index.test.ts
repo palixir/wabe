7540a714e9eef7f85f552b75f87552785406409f
coratgerl <73360179+coratgerl@users.noreply.github.com>
2024-09-06 21:27:41 +0200

    feat: add more test

--- packages/wabe-resend/src/index.test.ts
+++ packages/wabe-resend/src/index.test.ts
@@ -1,18 +1,22 @@
-import { describe, expect, it, mock, spyOn } from 'bun:test'
+import { describe, expect, it, mock, spyOn, beforeEach } from 'bun:test'
 import { ResendAdapter } from '.'
 import { Resend } from 'resend'
 
 describe('Resend', () => {
+  const mockSend = mock(() => {}).mockResolvedValueOnce({
+    data: { id: 'id' },
+    error: null,
+  } as never)
+
+  spyOn(Resend.prototype, 'emails').mockReturnValue({
+    send: mockSend,
+  } as never)
+
+  beforeEach(() => {
+    mockSend.mockClear()
+  })
+
   it('should send email with resend', async () => {
-    const mockSend = mock(() => {}).mockResolvedValueOnce({
-      data: { id: 'id' },
-      error: null,
-    } as never)
-
-    spyOn(Resend.prototype, 'emails').mockReturnValue({
-      send: mockSend,
-    } as never)
-
     const adapter = new ResendAdapter('FAKE_API_KEY')
 
     await adapter.send({
@@ -21,6 +25,34 @@ describe('Resend', () => {
       subject: 'Test',
       text: 'Content of the email',
     })
+
+    expect(mockSend).toHaveBeenCalledTimes(1)
+    expect(mockSend).toHaveBeenCalledWith({
+      from: 'test@wabe.dev',
+      to: ['delivered@resend.dev'],
+      subject: 'Test',
+      text: 'Content of the email',
+    })
+  })
+
+  it('should throw error if something wrong', async () => {
+    mockSend.mockResolvedValueOnce({
+      data: { id: 'id' },
+      error: {
+        message: 'error message',
+      },
+    } as never)
+
+    const adapter = new ResendAdapter('FAKE_API_KEY')
+
+    expect(
+      adapter.send({
+        from: 'test@wabe.dev',
+        to: ['delivered@resend.dev'],
+        subject: 'Test',
+        text: 'Content of the email',
+      }),
+    ).rejects.toThrow('error message')
 
     expect(mockSend).toHaveBeenCalledTimes(1)
     expect(mockSend).toHaveBeenCalledWith({
