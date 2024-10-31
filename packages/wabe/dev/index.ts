a5a082903860cc31fd75c234526d87358020896c
coratgerl <73360179+coratgerl@users.noreply.github.com>
2024-10-31 20:12:30 +0100

    fix: remove useless deps

--- packages/wabe/dev/index.ts
+++ packages/wabe/dev/index.ts
@@ -1,6 +1,5 @@
 import { runDatabase } from 'wabe-mongodb-launcher'
-import { StripeAdapter } from 'wabe-stripe'
-import { Currency, DatabaseEnum, Wabe } from '../src'
+import { DatabaseEnum, Wabe } from '../src'
 import type {
   WabeSchemaEnums,
   WabeSchemaScalars,
@@ -46,16 +45,6 @@ const run = async () => {
       name: 'Wabe',
     },
     port: 3000,
-    payment: {
-      adapter: new StripeAdapter(''),
-      currency: Currency.EUR,
-      supportedPaymentMethods: ['card'],
-      webhook: {
-        secret:
-          'whsec_56d339bf51e617aa4a3de35564a6bd5740517dbcfcd827226425bda49c033e5a',
-        url: '/webhooks/payment',
-      },
-    },
     schema: {
       classes: [
         {
