c19d735a15f4a77399891056485bd90e48abe066
coratgerl <73360179+coratgerl@users.noreply.github.com>
2024-12-19 17:24:30 +0100

    ci: try

--- packages/wabe/src/authentication/providers/Google.test.ts
+++ packages/wabe/src/authentication/providers/Google.test.ts
@@ -1,154 +1,166 @@
-// import { afterEach, describe, expect, it, mock, spyOn } from 'bun:test'
-// import { Google } from './Google'
-// biome-ignore lint/correctness/noUnusedImports: false
+import { afterEach, describe, expect, it, mock, spyOn } from 'bun:test'
+import { Google } from './Google'
 import { Google as GoogleOauth } from '../oauth/Google'
-// import { AuthenticationProvider } from '../interface'
+import { AuthenticationProvider } from '../interface'
 
-// describe('Google oauth', () => {
-//   const mockGetObjects = mock(() => Promise.resolve([]))
-//   const mockCount = mock(() => Promise.resolve(0)) as any
-//   const mockCreateObject = mock(() => Promise.resolve({ id: 'userId' })) as any
+describe('Google oauth', () => {
+  const mockGetObjects = mock(() => Promise.resolve([]))
+  const mockCount = mock(() => Promise.resolve(0)) as any
+  const mockCreateObject = mock(() => Promise.resolve({ id: 'userId' })) as any
 
-//   const mockValidateAuthorizationCode = spyOn(
-//     GoogleOauth.prototype,
-//     'validateAuthorizationCode',
-//   ).mockResolvedValue({
-//     idToken: 'idToken',
-//     accessToken: 'accessToken',
-//     refreshToken: 'refreshToken',
-//     accessTokenExpiresAt: new Date(0),
-//   })
+  const mockGetUserInfo = spyOn(
+    GoogleOauth.prototype,
+    'getUserInfo',
+  ).mockResolvedValue({
+    email: 'email@test.fr',
+    verifiedEmail: true,
+  })
 
-//   const mockGetUserInfo = spyOn(
-//     GoogleOauth.prototype,
-//     'getUserInfo',
-//   ).mockResolvedValue({
-//     email: 'email@test.fr',
-//     verifiedEmail: true,
-//   })
+  const context = {
+    wabe: {
+      controllers: {
+        database: {
+          getObjects: mockGetObjects,
+          createObject: mockCreateObject,
+          count: mockCount,
+        },
+      },
+      config: {
+        authentication: {
+          providers: {
+            google: {
+              clientId: 'clientId',
+              clientSecret: 'clientSecret',
+            },
+          },
+        },
+      },
+    },
+  } as any
 
-//   const context = {
-//     wabe: {
-//       controllers: {
-//         database: {
-//           getObjects: mockGetObjects,
-//           createObject: mockCreateObject,
-//           count: mockCount,
-//         },
-//       },
-//       config: {
-//         authentication: {
-//           providers: {
-//             google: {
-//               clientId: 'clientId',
-//               clientSecret: 'clientSecret',
-//             },
-//           },
-//         },
-//       },
-//     },
-//   } as any
+  afterEach(() => {
+    mockGetObjects.mockRestore()
+    mockCreateObject.mockClear()
+    mockCount.mockClear()
+    mockGetUserInfo.mockClear()
+  })
 
-//   afterEach(() => {
-//     mockGetObjects.mockClear()
-//     mockCreateObject.mockClear()
-//     mockCount.mockClear()
-//     mockValidateAuthorizationCode.mockClear()
-//     mockGetUserInfo.mockClear()
-//   })
+  it('should sign up with Google Provider if there is no user found', async () => {
+    const mockValidateAuthorizationCode = spyOn(
+      GoogleOauth.prototype,
+      'validateAuthorizationCode',
+    ).mockResolvedValue({
+      idToken: 'idToken',
+      accessToken: 'accessToken',
+      refreshToken: 'refreshToken',
+      accessTokenExpiresAt: new Date(0),
+    })
 
-//   it('should sign up with Google Provider if there is no user found', async () => {
-//     const google = new Google()
+    const google = new Google()
 
-//     await google.onSignIn({
-//       context,
-//       input: {
-//         authorizationCode: 'authorizationCode',
-//         codeVerifier: 'codeVerifier',
-//       },
-//     })
+    await google.onSignIn({
+      context,
+      input: {
+        authorizationCode: 'authorizationCode',
+        codeVerifier: 'codeVerifier',
+      },
+    })
 
-//     expect(mockValidateAuthorizationCode).toHaveBeenCalledTimes(1)
-//     expect(mockGetUserInfo).toHaveBeenCalledTimes(1)
+    expect(mockValidateAuthorizationCode).toHaveBeenCalledTimes(1)
+    expect(mockGetUserInfo).toHaveBeenCalledTimes(1)
 
-//     expect(mockGetObjects).toHaveBeenCalledTimes(1)
-//     expect(mockGetObjects).toHaveBeenCalledWith({
-//       className: 'User',
-//       where: {
-//         authentication: {
-//           google: {
-//             email: { equalTo: 'email@test.fr' },
-//           },
-//         },
-//       },
-//       first: 1,
-//       context: expect.any(Object),
-//       fields: ['id'],
-//     })
+    expect(mockGetObjects).toHaveBeenCalledTimes(1)
+    expect(mockGetObjects).toHaveBeenCalledWith({
+      className: 'User',
+      where: {
+        authentication: {
+          google: {
+            email: { equalTo: 'email@test.fr' },
+          },
+        },
+      },
+      first: 1,
+      context: expect.any(Object),
+      fields: ['id'],
+    })
 
-//     expect(mockCreateObject).toHaveBeenCalledTimes(1)
-//     expect(mockCreateObject).toHaveBeenCalledWith({
-//       className: 'User',
-//       data: {
-//         provider: AuthenticationProvider.Google,
-//         isOauth: true,
-//         authentication: {
-//           google: {
-//             email: 'email@test.fr',
-//             verifiedEmail: true,
-//             idToken: 'idToken',
-//           },
-//         },
-//       },
-//       context: expect.any(Object),
-//       fields: ['*'],
-//     })
-//   })
+    expect(mockCreateObject).toHaveBeenCalledTimes(1)
+    expect(mockCreateObject).toHaveBeenCalledWith({
+      className: 'User',
+      data: {
+        provider: AuthenticationProvider.Google,
+        isOauth: true,
+        authentication: {
+          google: {
+            email: 'email@test.fr',
+            verifiedEmail: true,
+            idToken: 'idToken',
+          },
+        },
+      },
+      context: expect.any(Object),
+      fields: ['*'],
+    })
 
-//   it('should sign in with Google Provider if there is no user found', async () => {
-//     mockGetObjects.mockResolvedValue([
-//       {
-//         id: 'userId',
-//         authentication: {
-//           google: {
-//             email: 'email@test.fr',
-//             verifiedEmail: true,
-//             idToken: 'idToken',
-//           },
-//         },
-//         provider: AuthenticationProvider.Google,
-//         isOauth: true,
-//       } as any,
-//     ] as never)
+    mockValidateAuthorizationCode.mockRestore()
+  })
 
-//     const google = new Google()
+  it('should sign in with Google Provider if there is no user found', async () => {
+    const mockValidateAuthorizationCode = spyOn(
+      GoogleOauth.prototype,
+      'validateAuthorizationCode',
+    ).mockResolvedValue({
+      idToken: 'idToken',
+      accessToken: 'accessToken',
+      refreshToken: 'refreshToken',
+      accessTokenExpiresAt: new Date(0),
+    })
 
-//     await google.onSignIn({
-//       context,
-//       input: {
-//         authorizationCode: 'authorizationCode',
-//         codeVerifier: 'codeVerifier',
-//       },
-//     })
+    mockGetObjects.mockResolvedValue([
+      {
+        id: 'userId',
+        authentication: {
+          google: {
+            email: 'email@test.fr',
+            verifiedEmail: true,
+            idToken: 'idToken',
+          },
+        },
+        provider: AuthenticationProvider.Google,
+        isOauth: true,
+      } as any,
+    ] as never)
 
-//     expect(mockValidateAuthorizationCode).toHaveBeenCalledTimes(1)
-//     expect(mockGetUserInfo).toHaveBeenCalledTimes(1)
+    const google = new Google()
 
-//     expect(mockGetObjects).toHaveBeenCalledTimes(1)
-//     expect(mockGetObjects).toHaveBeenCalledWith({
-//       className: 'User',
-//       where: {
-//         authentication: {
-//           google: {
-//             email: { equalTo: 'email@test.fr' },
-//           },
-//         },
-//       },
-//       first: 1,
-//       context: expect.any(Object),
-//       fields: ['id'],
-//     })
+    await google.onSignIn({
+      context,
+      input: {
+        authorizationCode: 'authorizationCode',
+        codeVerifier: 'codeVerifier',
+      },
+    })
 
-//     expect(mockCreateObject).toHaveBeenCalledTimes(0)
-//   })
-// })
+    expect(mockValidateAuthorizationCode).toHaveBeenCalledTimes(1)
+    expect(mockGetUserInfo).toHaveBeenCalledTimes(1)
+
+    expect(mockGetObjects).toHaveBeenCalledTimes(1)
+    expect(mockGetObjects).toHaveBeenCalledWith({
+      className: 'User',
+      where: {
+        authentication: {
+          google: {
+            email: { equalTo: 'email@test.fr' },
+          },
+        },
+      },
+      first: 1,
+      context: expect.any(Object),
+      fields: ['id'],
+    })
+
+    expect(mockCreateObject).toHaveBeenCalledTimes(0)
+
+    mockValidateAuthorizationCode.mockRestore()
+  })
+})
