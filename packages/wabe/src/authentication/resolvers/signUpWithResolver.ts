1b67885a5db89d7b8647919434730fb8c46e560d
coratgerl <73360179+coratgerl@users.noreply.github.com>
2025-01-05 19:52:03 +0100

    fix(wabe): authorize root access for signUp

--- packages/wabe/src/authentication/resolvers/signUpWithResolver.ts
+++ packages/wabe/src/authentication/resolvers/signUpWithResolver.ts
@@ -22,7 +22,10 @@ export const signUpWithResolver = async (
   // TODO: improve this maybe need a refactor of ACL interface
   // Fix to allow anonymous user to create user but not when user creation is blocked for anonymous
   // Because here the createObject is done with root to avoid ACL issues
-  if (userSchema?.permissions?.create?.requireAuthentication === true)
+  if (
+    userSchema?.permissions?.create?.requireAuthentication === true &&
+    !context.isRoot
+  )
     throw new Error('Permission denied to create class User')
 
   // Create object call the provider signUp
