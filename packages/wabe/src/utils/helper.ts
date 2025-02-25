27c9503a7224505b3cc8c96059e9dd2775d45b81
coratgerl <mr.palixir@gmail.com>
2025-02-25 21:16:47 +0100

    fix: remove useless type

--- packages/wabe/src/utils/helper.ts
+++ packages/wabe/src/utils/helper.ts
@@ -16,20 +16,6 @@ import { EmailDevAdapter } from '../email/DevAdapter'
 import { Currency } from '../payment'
 import { FileDevAdapter } from '../files/FileDevAdapter'
 
-type NotNull<T> = T extends null | undefined ? never : T
-
-type Primitive = undefined | null | boolean | string | number
-
-export type DeepRequired<T> = T extends Primitive
-  ? NotNull<T>
-  : {
-      [P in keyof T]-?: T[P] extends Array<infer U>
-        ? Array<DeepRequired<U>>
-        : T[P] extends ReadonlyArray<infer U2>
-          ? DeepRequired<U2>
-          : DeepRequired<T[P]>
-    }
-
 export interface DevWabeTypes extends WabeTypes {
   types: WabeSchemaTypes
   scalars: WabeSchemaScalars
