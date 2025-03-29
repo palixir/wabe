20aaca0e74138b0a9e1f3f5e5123d60e9ed017b2
coratgerl <73360179+coratgerl@users.noreply.github.com>
2025-03-29 09:33:33 +0100

    feat(wabe-postgres-launcher): create postgres launcher

--- packages/wabe-postgres-launcher/src/index.ts
+++ packages/wabe-postgres-launcher/src/index.ts
@@ -1,8 +1,8 @@
+import type { Client } from 'pg'
 import { newDb } from 'pg-mem'
 import tcpPortUsed from 'tcp-port-used'
-import { PostgresMock } from 'pgmock'
 
-export const runDatabase = async () => {
+export const runDatabase = async (): Promise<Client | undefined> => {
   const port = 5432
   const universalPort = '127.0.0.1'
   try {
@@ -10,9 +10,11 @@ export const runDatabase = async () => {
     const db = newDb()
 
     console.info('PostgreSQL started')
+
+    const { Client } = db.adapters.createPg()
+
+    return Client
   } catch (error) {
     console.error('Error setting up in-memory PostgreSQL:', error)
   }
 }
-
-runDatabase()
