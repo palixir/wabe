7a48d61ad32d5dbee593015848eeb048e852ef9d
coratgerl <73360179+coratgerl@users.noreply.github.com>
2025-04-06 20:58:29 +0200

    feat: improve dashboard base

--- packages/wabe-dashboard/nuxt.config.ts
+++ packages/wabe-dashboard/nuxt.config.ts
@@ -1,26 +1,17 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devServer: {
-    port: 3000,
+  modules: ['@nuxt/ui'],
+  devtools: {
+    enabled: true,
   },
-  compatibilityDate: '2024-11-01',
-  devtools: { enabled: true },
-  modules: [
-    '@nuxtjs/tailwindcss',
-    '@nuxt/ui',
-    '@nuxtjs/storybook',
-    '@nuxtjs/apollo',
-  ],
-  storybook: {
-    route: '/__storybook__',
-    port: 6006,
-  },
-  apollo: {
-    clients: {
-      default: {
-        connectToDevTools: true,
-        httpEndpoint: 'http://127.0.0.1:3001/graphql',
-      },
+  css: ['~/assets/css/main.css'],
+  routeRules: {
+    '/api/**': {
+      cors: true,
     },
   },
+  future: {
+    compatibilityVersion: 4,
+  },
+  compatibilityDate: '2024-07-11',
 })
---
+++ packages/wabe-dashboard/pages/app.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="bg-red-500">
+    tataa
+  </div>
+</template>
