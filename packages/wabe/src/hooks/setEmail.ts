e1f39a912dea11d886b3e1e331032857abc96c1f
sukrucildirr <sukrucildirr@gmail.com>
2025-06-01 18:22:45 +0300

    Update setEmail.ts

--- packages/wabe/src/hooks/setEmail.ts
+++ packages/wabe/src/hooks/setEmail.ts
@@ -6,7 +6,7 @@ const updateEmail = (object: HookObject<DevWabeTypes, 'User'>) => {
 
   if (!authentication) return
 
-  // Considering that we only have one authentication provider (for double auth maybe need an ajustment
+  // Considering that we only have one authentication provider (for double auth maybe need an adjustment)
   const provider = Object.keys(authentication)[0]
 
   const emailToSave = authentication[provider].email
