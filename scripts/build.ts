dec44dbb11bf114a84a2177a8d10793826446f99
coratgerl <73360179+coratgerl@users.noreply.github.com>
2025-06-08 09:33:47 +0200

    feat: stabilizate ci

---
+++ scripts/build.ts
@@ -0,0 +1,29 @@
+import { $ } from 'bun'
+import { readdir } from 'node:fs/promises'
+
+const packages = await readdir('./packages', { withFileTypes: true })
+
+const excludedPackages = ['wabe-build', 'wabe-documentation']
+const otherPackagesToRunBeforeMainPackages = [
+  'wabe-mongodb-launcher',
+  'wabe-postgres-launcher',
+  'wabe',
+]
+
+const packagesWithoutDatabaseLaunchersAndExcludedPackages = packages.filter(
+  (packageDirectory) =>
+    ![...excludedPackages, ...otherPackagesToRunBeforeMainPackages].includes(
+      packageDirectory.name,
+    ),
+)
+
+const firstCommand = otherPackagesToRunBeforeMainPackages
+  .map((name) => `bun --filter ./packages/${name} build`)
+  .join(' && ')
+
+const secondCommand = packagesWithoutDatabaseLaunchersAndExcludedPackages
+  .map(({ name }) => `bun --filter ./packages/${name} build`)
+  .join(' && ')
+
+await $`sh -c ${firstCommand}`
+await $`sh -c ${secondCommand}`
---
+++ scripts/ci.ts
@@ -0,0 +1,32 @@
+import { $ } from 'bun'
+import tcpPortUsed from 'tcp-port-used'
+import { readdir } from 'node:fs/promises'
+
+const packages = await readdir('./packages', { withFileTypes: true })
+
+const excludedPackages = ['wabe-build', 'wabe-documentation']
+const otherPackagesToRunAfterMainPackages = ['wabe-mongodb', 'wabe-postgres']
+
+const packagesWithoutDatabaseAdapters = packages.filter(
+  (packageDirectory) =>
+    ![...excludedPackages, ...otherPackagesToRunAfterMainPackages].includes(
+      packageDirectory.name,
+    ),
+)
+
+const firstCommand = packagesWithoutDatabaseAdapters
+  .map(({ name }) => `bun --filter ./packages/${name} ci`)
+  .join(' && ')
+
+// Include wabe-mongodb and wabe-postgres
+const secondCommand = otherPackagesToRunAfterMainPackages
+  .map((name) => `bun --filter ./packages/${name} ci`)
+  .join(' && ')
+
+await $`sh -c ${firstCommand}`
+
+console.log('⏳ Waiting for port 27045 (for mongodb) to be free...')
+await tcpPortUsed.waitUntilFree(27045, 100, 30000)
+console.log('✅ Port 27045 is free.')
+
+await $`sh -c ${secondCommand}`
