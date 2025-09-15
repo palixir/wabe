ce9a23b79f743dae96170ee585910d779e227815
coratgerl <73360179+coratgerl@users.noreply.github.com>
2025-09-15 20:43:15 +0200

    fix: remove test files

--- packages/wabe/titi.ts
+++
@@ -1,5 +0,0 @@
-const tata =
-  '$argon2id$v=19$m=65536,t=2,p=1$ta0LmQzammQDSlQbkC9Oug$E+5f7J1HrSmIMQ/9EI2IexFzSPTAT4iHQF91ErtlXgl5OZQWN9iHZE9NFjeMHGemBlrhcezFzUK0xt83OJw5yw'
-
-console.log(await Bun.password.hash('monmotdepasse', { algorithm: 'argon2id' }))
-console.log(await Bun.password.verify('monmotderpasse', tata))
