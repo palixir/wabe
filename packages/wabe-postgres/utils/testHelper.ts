79b39985ed0adb54ddf752268a2a0d33adc81a32
Lucas Coratger <73360179+coratgerl@users.noreply.github.com>
2025-11-10 16:14:32 +0100

    fix: timeout

--- packages/wabe-postgres/utils/testHelper.ts
+++ packages/wabe-postgres/utils/testHelper.ts
@@ -5,120 +5,120 @@ import getPort from 'get-port'
 import { PostgresAdapter } from '../src'
 
 export const setupTests = async (
-  additionalClasses: ClassInterface<any>[] = [],
+	additionalClasses: ClassInterface<any>[] = []
 ) => {
-  const databaseId = uuid()
+	const databaseId = uuid()
 
-  const port = await getPort()
+	const port = await getPort()
 
-  await runDatabase()
+	await runDatabase()
 
-  const wabe = new Wabe<any>({
-    isProduction: false,
-    rootKey:
-      '0uwFvUxM$ceFuF1aEtTtZMa7DUN2NZudqgY5ve5W*QCyb58cwMj9JeoaV@d#%29v&aJzswuudVU1%nAT+rxS0Bh&OkgBYc0PH18*',
-    database: {
-      adapter: new PostgresAdapter({
-        databaseUrl: 'postgresql://wabe:wabe@localhost:5432',
-        databaseName: databaseId,
-      }),
-    },
-    authentication: {
-      roles: ['Client', 'Client2', 'Client3', 'Admin'],
-      session: {
-        jwtSecret: 'dev',
-        cookieSession: true,
-      },
-    },
-    port,
-    schema: {
-      classes: [
-        ...additionalClasses,
-        {
-          name: 'Test',
-          fields: {
-            field1: { type: 'String' },
-            int: { type: 'Int' },
-            array: { type: 'Array', typeValue: 'String' },
-            date: { type: 'Date' },
-            enum: { type: 'AuthenticationProvider' },
-            object: {
-              type: 'Object',
-              object: {
-                name: 'Object',
-                fields: {
-                  array: {
-                    type: 'Array',
-                    typeValue: 'Object',
-                    object: {
-                      name: 'SubObject',
-                      fields: {
-                        string: {
-                          type: 'String',
-                        },
-                      },
-                    },
-                  },
-                },
-              },
-            },
-          },
-        },
-        {
-          name: 'User',
-          fields: {
-            name: { type: 'String' },
-            age: { type: 'Int' },
-            isAdmin: { type: 'Boolean', defaultValue: false },
-            floatValue: { type: 'Float' },
-            birthDate: { type: 'Date' },
-            arrayValue: {
-              type: 'Array',
-              typeValue: 'String',
-            },
-            test: { type: 'TestScalar' },
-          },
-          searchableFields: ['email'],
-          permissions: {
-            create: {
-              requireAuthentication: false,
-            },
-            delete: {
-              requireAuthentication: true,
-            },
-            update: {
-              requireAuthentication: false,
-            },
-            read: {
-              requireAuthentication: false,
-            },
-            acl: async (hookObject) => {
-              await hookObject.addACL('users', {
-                userId: hookObject.object?.id,
-                read: true,
-                write: true,
-              })
+	const wabe = new Wabe<any>({
+		isProduction: false,
+		rootKey:
+			'0uwFvUxM$ceFuF1aEtTtZMa7DUN2NZudqgY5ve5W*QCyb58cwMj9JeoaV@d#%29v&aJzswuudVU1%nAT+rxS0Bh&OkgBYc0PH18*',
+		database: {
+			adapter: new PostgresAdapter({
+				databaseUrl: 'postgresql://wabe:wabe@localhost:5432',
+				databaseName: databaseId,
+			}),
+		},
+		authentication: {
+			roles: ['Client', 'Client2', 'Client3', 'Admin'],
+			session: {
+				jwtSecret: 'dev',
+				cookieSession: true,
+			},
+		},
+		port,
+		schema: {
+			classes: [
+				...additionalClasses,
+				{
+					name: 'Test',
+					fields: {
+						field1: { type: 'String' },
+						int: { type: 'Int' },
+						array: { type: 'Array', typeValue: 'String' },
+						date: { type: 'Date' },
+						enum: { type: 'AuthenticationProvider' },
+						object: {
+							type: 'Object',
+							object: {
+								name: 'Object',
+								fields: {
+									array: {
+										type: 'Array',
+										typeValue: 'Object',
+										object: {
+											name: 'SubObject',
+											fields: {
+												string: {
+													type: 'String',
+												},
+											},
+										},
+									},
+								},
+							},
+						},
+					},
+				},
+				{
+					name: 'User',
+					fields: {
+						name: { type: 'String' },
+						age: { type: 'Int' },
+						isAdmin: { type: 'Boolean', defaultValue: false },
+						floatValue: { type: 'Float' },
+						birthDate: { type: 'Date' },
+						arrayValue: {
+							type: 'Array',
+							typeValue: 'String',
+						},
+						test: { type: 'TestScalar' },
+					},
+					searchableFields: ['email'],
+					permissions: {
+						create: {
+							requireAuthentication: false,
+						},
+						delete: {
+							requireAuthentication: true,
+						},
+						update: {
+							requireAuthentication: false,
+						},
+						read: {
+							requireAuthentication: false,
+						},
+						acl: async (hookObject) => {
+							await hookObject.addACL('users', {
+								userId: hookObject.object?.id,
+								read: true,
+								write: true,
+							})
 
-              await hookObject.addACL('roles', null)
-            },
-          },
-        },
-      ],
-      scalars: [
-        {
-          name: 'TestScalar',
-          description: 'Test scalar',
-        },
-      ],
-    },
-  })
+							await hookObject.addACL('roles', null)
+						},
+					},
+				},
+			],
+			scalars: [
+				{
+					name: 'TestScalar',
+					description: 'Test scalar',
+				},
+			],
+		},
+	})
 
-  await wabe.start()
+	await wabe.start()
 
-  return { wabe, port }
+	return { wabe, port }
 }
 
 export const closeTests = async (wabe: Wabe<any>) => {
-  await wabe?.controllers?.database?.adapter?.close()
-  await wabe?.close()
+	await wabe.controllers?.database?.adapter?.close()
+	await wabe.close()
 }
