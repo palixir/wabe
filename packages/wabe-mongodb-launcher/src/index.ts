a666de3fa79331ba4321dbeec02870fab4c2ba54
Palixir <73360179+coratgerl@users.noreply.github.com>
2026-02-22 09:47:01 +0100

    feat(wabe-mongodb-launcher): improve launcher and add options (#306)

--- packages/wabe-mongodb-launcher/src/index.ts
+++ packages/wabe-mongodb-launcher/src/index.ts
@@ -1,23 +1,27 @@
 import { MongoMemoryServer } from 'mongodb-memory-server'
 import tcpPortUsed from 'tcp-port-used'
-import PQueue from 'p-queue'
 
-const queue = new PQueue({ concurrency: 1 })
+type RunDatabaseOptions = {
+	binaryVersion?: string
+}
 
-export const runDatabase = async (): Promise<void> =>
-	queue.add(async () => {
-		await startMongo()
-	})
+export const runDatabase = async (
+	port: number = 27045,
+	options: RunDatabaseOptions = {},
+): Promise<void> => startMongo(port, options)
 
-const startMongo = async (): Promise<void> => {
-	if (await tcpPortUsed.check(27045, '127.0.0.1')) return
+const startMongo = async (
+	port: number,
+	{ binaryVersion = '8.2.1' }: RunDatabaseOptions = {},
+): Promise<void> => {
+	if (await tcpPortUsed.check(port, '127.0.0.1')) return
 
 	await MongoMemoryServer.create({
 		binary: {
-			version: '8.0.5',
+			version: binaryVersion,
 		},
 		instance: {
-			port: 27045,
+			port,
 		},
 	})
 
